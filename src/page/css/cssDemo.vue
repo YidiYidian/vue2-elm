dbd84fb3ff2df4fea124b17cb12d9791d1cfa34e
YIdiyidian <1217128846@qq.com>
2021-01-06 23:47:24 +0800

    水平布局demo

--- src/page/css/cssDemo.vue
+++ src/page/css/cssDemo.vue
@@ -1,22 +1,72 @@
 <template>
-    <div class="fillcontain">
-        <p class="explain_text">node-elm后台管理系统</p>
-        <p class="explain_text">第一次登录的用户自动注册成为普通管理员</p>
-        <p class="explain_text">普通管理员可以添加，修改信息</p>
-        <p class="explain_text">超级管理员可以删除信息</p>
+  <div class="fillcontain">
+    <div class="swiper-container">
+      <div class="food_types_container">
+        <div class="link_to_food">
+          <figure>
+            <img src="../../images/qq.png" />
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+          </figure>
+          <figure>
+            <img src="../../images/qq.png" />
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+          </figure>
+          <figure>
+            <img src="../../images/qq.png" />
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+          </figure>
+          <figure>
+            <img src="../../images/qq.png" />
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+            <figcaption>图片图片</figcaption>
+          </figure>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {};
 </script>
 
-<style lang="less">
-	.explain_text{
-		margin-top: 20px;
-		text-align: center;
-		font-size: 20px;
-		color: #333;
-	}
+<style lang="less" scoped>
+.swiper-container {
+  width: 100%;
+  height: auto;
+  padding-bottom: 0.6rem;
+  .swiper-pagination {
+    bottom: 0.2rem;
+  }
+}
+.food_types_container {
+  display: flex;
+  flex-wrap: wrap;
+  .link_to_food {
+    width: 100%;
+    padding: 0.3rem 0rem;
+    display: flex;
+    justify-content: center;
+    figure {
+      margin: 0 35px;
+      img {
+        margin-bottom: 0.3rem;
+        width: 1.8rem;
+        height: 1.8rem;
+      }
+      figcaption {
+        font-size: 35px;
+        text-align: center;
+        color: rgb(197, 60, 60);
+      }
+    }
+  }
+}
 </style>
